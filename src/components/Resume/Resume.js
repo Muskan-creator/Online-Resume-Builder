8d3d623684d130cdd4901666f436532f312123a2
Muskan <59757988+Muskan-creator@users.noreply.github.com>
2023-01-05 20:04:01 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -294,7 +294,7 @@ const Resume = forwardRef((props, ref) => {
   }, []);
 
   useEffect(() => {
-    swapSourceTarget(source, target);
+    swapSourceTarget(source, target)
     
   }, [source]);
 
